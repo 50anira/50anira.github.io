00870dbccca404189039cf82dc60f10ac2aa4eea
50anira <ronald.janki@gmail.com>
2025-09-27 10:51:15 -0400

    Python code updates, index page updated and created new gallery.html

--- app.py
+++ app.py
@@ -6,14 +6,21 @@ from dotenv import load_dotenv
 load_dotenv()
 app = Flask(__name__)
 
-# Load all four of your permanent credentials from Render's Environment Variables
-CONSUMER_KEY = os.getenv("SMUGMUG_API_KEY")
-CONSUMER_SECRET = os.getenv("SMUGMUG_API_SECRET")
-ACCESS_TOKEN = os.getenv("SMUGMUG_ACCESS_TOKEN")
-ACCESS_TOKEN_SECRET = os.getenv("SMUGMUG_ACCESS_TOKEN_SECRET")
+# Load all four of your permanent credentials
+CONSUMER_KEY = os.getenv("SMUGMug_API_KEY")
+CONSUMER_SECRET = os.getenv("SMUGMug_API_SECRET")
+ACCESS_TOKEN = os.getenv("SMUGMug_ACCESS_TOKEN")
+ACCESS_TOKEN_SECRET = os.getenv("SMUGMug_ACCESS_TOKEN_SECRET")
 
+# --- Homepage Route ---
 @app.route('/')
 def home():
+    # This page will now just show a simple template
+    return render_template('index.html')
+
+# --- Secret Gallery Route ---
+@app.route('/gallery/<album_key>')
+def gallery(album_key):
     oauth = OAuth1Session(
         CONSUMER_KEY,
         client_secret=CONSUMER_SECRET,
@@ -22,21 +29,19 @@ def home():
     )
 
     try:
-        profile_response = oauth.get("https://api.smugmug.com/api/v2!authuser", headers={'Accept': 'application/json'})
-        profile_response.raise_for_status()
-        nickname = profile_response.json()['Response']['User']['NickName']
+        # Construct the URL to get a specific album's images
+        # We also expand the ImageSizeDetails to get a good thumbnail URL
+        images_url = f"https://api.smugmug.com/api/v2/album/{album_key}!images?_expand=ImageSizeDetails"
         
-        # Add the _expand parameter to the URL to request the cover image
-        albums_url = f"https://api.smugmug.com/api/v2/user/{nickname}!albums?count=100&_expand=AlbumCoverImage"
+        images_response = oauth.get(images_url, headers={'Accept': 'application/json'})
+        images_response.raise_for_status()
         
-        albums_response = oauth.get(albums_url, headers={'Accept': 'application/json'})
-        albums_response.raise_for_status()
+        # Get the list of images from the response
+        images = images_response.json()['Response']['AlbumImage']
         
-        all_albums = albums_response.json()['Response']['Album']
-        public_albums = [album for album in all_albums if album.get('SecurityType') != 'Password']
-        
-        return render_template('index.html', albums=public_albums)
+        # Pass the list of images to a new gallery template
+        return render_template('gallery.html', images=images)
 
     except Exception as e:
-        print(f"An error occurred during API call: {e}")
-        return "Sorry, there was an error fetching data from SmugMug. Check the logs for details.", 500
+        print(f"An error occurred: {e}")
+        return "Sorry, there was an error fetching the album from SmugMug.", 500
