7c7413ba60184f8e54a904dec2f88f953233de3b
50anira <ronald.janki@gmail.com>
2025-09-26 20:54:46 -0400

    Update app.py

--- app.py
+++ app.py
@@ -64,6 +64,23 @@ def callback():
         return f"Connected! Nickname: {nickname}<br>Access Token: {access_token}<br>Access Token Secret: {access_token_secret}"
     else:
         return "Failed to fetch user profile", 500
+    
+# --- Route to call Albums
+@app.route('/albums')
+def albums():
+    access_token = session.get('access_token')
+    access_token_secret = session.get('access_token_secret')
+
+    if not access_token or not access_token_secret:
+        return redirect('/')
+
+    albums_data = get_user_albums(access_token, access_token_secret)
+
+    if albums_data:
+        album_titles = [album['Title'] for album in albums_data['Response']['Album']]
+        return "<h2>Your Albums:</h2>" + "<br>".join(album_titles)
+    else:
+        return "Could not fetch albums", 500
 
 
 # --- Function to Fetch User Profile ---
@@ -81,6 +98,29 @@ def get_user_profile(access_token, access_token_secret):
         return response.json()
     else:
         print("Error:", response.status_code, response.text)
+        return None
+
+# --- Function to Fetch Albums
+def get_user_albums(access_token, access_token_secret):
+    oauth = OAuth1Session(
+        CONSUMER_KEY,
+        client_secret=CONSUMER_SECRET,
+        resource_owner_key=access_token,
+        resource_owner_secret=access_token_secret
+    )
+
+    # First, get the nickname
+    profile = oauth.get("https://api.smugmug.com/api/v2!authuser").json()
+    nickname = profile['Response']['User']['NickName']
+
+    # Now fetch albums
+    albums_url = f"https://api.smugmug.com/api/v2/user/{nickname}!albums"
+    response = oauth.get(albums_url)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print("Error fetching albums:", response.status_code, response.text)
         return None
 
 
